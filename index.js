0a7fe2c7275e8c8a271461891c9c053bcdbf8e17
Jason Ours <jasonours@outlook.com>
2020-02-12 18:50:54 -0600

    six passing

--- index.js
+++ index.js
@@ -40,13 +40,13 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  let luke = {
-    id: 1,
-    name: "Luke",
-    email: "luke@luke.com",
+function makePersonObject(newId, newName, newEmail) {
+  let newPerson = {
+    id: newId,
+    name: newName,
+    email: newEmail,
   }
-  return luke;
+  return newPerson;
 }
 
 /**
@@ -62,8 +62,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  let name = "Luke";
+function getName(nameObject) {
+  let name = nameObject.name;
   
   return("Hello, my name is " +  `${name}.`)
 }
@@ -81,8 +81,17 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(nameArgu) {
+  let nameObject = {
+    name: nameArgu,
+    sum: function(num1, num2){
+      return num1 + num2;
+    },
+    speak: function(){
+      return "Hello, my name is " +  this.name;
+    },
+  };
+  return nameObject;     
 }
 
 
@@ -144,7 +153,7 @@ function get3rdCar(inventory) {
  * it will return `This is a Lincoln Navigator`.
 */
 function getCarInfoByIndex(inventory, index) {
-  /* code here */
+  
 }
 
 /**
